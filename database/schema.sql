609ef5e32867b45dc9539840395788d89b90e0b9
akarthik10 <akarthik101@gmail.com>
2015-02-24 23:37:45 +0530

    Fixed schema dependency

--- database/schema.sql
+++ database/schema.sql
@@ -2959,35 +2959,6 @@ ALTER TABLE `rights`
 
 
 
--- Custom tables
-
--- phpMyAdmin SQL Dump
--- version 3.2.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Feb 22, 2015 at 04:45 PM
--- Server version: 5.1.41
--- PHP Version: 5.3.1
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `testdb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `expense`
---
-
 CREATE TABLE IF NOT EXISTS `expense` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(25) NOT NULL,
@@ -3003,6 +2974,10 @@ CREATE TABLE IF NOT EXISTS `expense` (
 -- Dumping data for table `expense`
 --
 
+CREATE TABLE IF NOT EXISTS `expense_type` (
+  `expense_category` varchar(25) NOT NULL,
+  PRIMARY KEY (`expense_category`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Constraints for dumped tables
@@ -3046,10 +3021,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `expense_type`
 --
 
-CREATE TABLE IF NOT EXISTS `expense_type` (
-  `expense_category` varchar(25) NOT NULL,
-  PRIMARY KEY (`expense_category`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
