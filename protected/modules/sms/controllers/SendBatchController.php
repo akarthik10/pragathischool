d28ff6c35680fd70eaff876319351bbfbf11360f
akarthik10 <akarthik101@gmail.com>
2015-02-25 21:16:44 +0530

    SendSMSBatch case fixed

--- protected/modules/sms/controllers/SendBatchController.php
+++ protected/modules/sms/controllers/SendBatchController.php
@@ -1,6 +1,6 @@
 <?php
 
-class SendBatchController extends RController
+class SendbatchController extends RController
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
